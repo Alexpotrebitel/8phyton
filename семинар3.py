f99d9bdb9c1754784e62e29f3a8a09ca3eff0745
Alexpotrebitel <118938387+Alexpotrebitel@users.noreply.github.com>
2023-04-06 00:13:16 +0500

    3r

--- семинар3.py
+++ семинар3.py
@@ -55,18 +55,62 @@
 # Input: [{"V": "S001", "X": "D009"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005", "XI": "D011"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 ", "XII": "D001"}]
 
 # Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
-my_dict=[
-    {"V": "S001", "X": "D009"},
-    {"V": "S002"},
-    {"VI": "S001"}, 
-    {"VI": "S005", "XI": "D011"},
-    {"VII": " S005 "},
-    {" V ":" S009 "},
-    {" VIII ":" S007 ", "XII": "D001"}]
+# my_dict=[
+#     {"V": "S001", "X": "D009"},
+#     {"V": "S002"},
+#     {"VI": "S001"}, 
+#     {"VI": "S005", "XI": "D011"},
+#     {"VII": " S005 "},
+#     {" V ":" S009 "},
+#     {" VIII ":" S007 ", "XII": "D001"}]
 
-my_set=set()
-for elements in my_dict:
-        for elem in elements.values():
-            my_set.add(elem.strip)
-print(my_set)
+# my_set=set()
+# for elements in my_dict:
+#         for elem in elements.values():
+#             my_set.add(elem.strip)
+# print(my_set)
 #
+#print([elem for elem in[list(element.values())for element in my_dict]]) #другой вариант
+#print([{elem.strip(): elem.strip() for element in my_dict for elem in element.values()}])
+#print([{key: value.strip() for element in my_dict for elem in element.items()}])
+
+#2
+# my_list = [
+# {"V": "S001", "X": "D009"},
+# {"V": "S002"},
+# {"VI": "S001"},
+# {"VI": "S005", "XI": "D011"},
+# {"VII": " S005 "},
+# {" V ": " S009 "},
+# {" VIII ": " S007 ", "XII": "D001"}
+# ]
+
+# # for element in my_list:
+# # for elem in element.values():
+# # my_set.add(elem.strip())
+# #
+# # print(my_set)
+
+# # for key, val in {'a': 14}:
+# # pass
+
+# print({elem.strip(): elem.strip() for element in my_list for elem in element.values()})
+
+# Дан список, состоящий из целых чисел. Напишите программу, которая сформирует список из тех элементов, которые больше предыдущего (элемента с предыдущим номером)
+# Примечание: Пользователь может вводить значения списка или список задан изначально.
+# Примеры/Тесты:
+# Input: [0, -1, 5, 2, 1]
+# Output: [5]
+
+# Input: [-2, -1, 5, 2, 3]
+# Output: [-1, 5, 3]
+# [*] Усложнение: Запишите алгоритм в одну строку, используйте Comprehension
+element=[0, -1, 5, 2, 1]
+max_element=[]
+# for index_element in range(len(element)):
+#     if element[index_element]>element[index_element-1]:
+#         max_element.append(element[index_element])
+# print(max_element)
+#
+# в одну строчку
+print (element for index_elements in range(len(element)) if element[index_elements] > element[index_elements -1])
