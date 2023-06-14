51b43f287999c70cc90d2a6afa180e4769e0ba7e
Alexpotrebitel <118938387+Alexpotrebitel@users.noreply.github.com>
2023-06-14 21:48:43 +0500

    Add files via upload

--- 7 seminar.py
+++ 7 seminar.py
@@ -84,28 +84,49 @@
 
 # for fio in data:
 #     new_fio=""
-#     for idx, el in enumerate(fio):
+# #     for idx, el in enumerate(fio):
         
-#         if idx!=0:
-#             new_fio+=' '+el[0]+'.'
-#         else:
-#             new_fio+=el+' '
-#     new_data.append(new_fio) 
+# #         if idx!=0:
+# #             new_fio+=' '+el[0]+'.'
+# #         else:
+# #             new_fio+=el+' '
+# #     new_data.append(new_fio) 
      
-# print (new_data)
-#2 вариант
-from os.path import join
-datapath=join(".", "семинары")
-with open(join(datapath, "7seminar.csv"), mode='r', encoding='utf-8') as data_file:
-  data = [line.strip().split('#') for line in data_file]
-new_data=list()
+# # print (new_data)
+# #2 вариант
+# from os.path import join
+# datapath=join(".", "семинары")
+# with open(join(datapath, "7seminar.csv"), mode='r', encoding='utf-8') as data_file:
+#   data = [line.strip().split('#') for line in data_file]
+# new_data=list()
 
-for fio in data:
-        new_fio=f"{fio[0]},{fio[1][0]},{fio[2][0]}"
-        new_data.append(new_fio) 
-# new_data2=[f"{fio[0]},{fio[1][0]},{fio[2][0]}"for fio in data]
-new_data2=[f"{surname}{name[0]},{parent[0]}"for surname, name, parent in data]
-print (new_data)
+# for fio in data:
+#         new_fio=f"{fio[0]},{fio[1][0]},{fio[2][0]}"
+#         new_data.append(new_fio) 
+# # new_data2=[f"{fio[0]},{fio[1][0]},{fio[2][0]}"for fio in data] #ниже уже есть оставить ковычки
+# new_data2=[f"{surname}{name[0]},{parent[0]}"for surname, name, parent in data]
+# # with open(join(datapath,"result","7seminar.csv"), mode='r', encoding='utf-8') as data_file:
+# #   [data_file.write(el+"\n") for el in new_data2]
+# print (*new_data2, sep="\n")
+
+#vs code
+# from pathlib import Path
+# from os import getcwd
+# from os.path import join, abspath, dirname
+
+# MAIN_DIR_OP1 = abspath(dirname(__file__))
+# MAIN_DIR_OP2 = abspath(dirname("."))
+
+# MAIN_DIR_PL1 = Path(__file__).parent
+# MAIN_DIR_PL2 = Path(".")
+
+# print(f"текущая директория: {getcwd()}")
+# print("PATHLIB")
+# print(f"Через __file__: {MAIN_DIR_PL1.absolute()}")
+# print(f"Через '.': {MAIN_DIR_PL2.absolute()}")
+# print("OS.PATH")
+# print(f"Через __file__: {MAIN_DIR_OP1}")
+# print(f"Через '.': {MAIN_DIR_OP1}")
 
 #чат сделал
 # from os.path import join
@@ -138,8 +159,24 @@ print (new_data)
 
 # # [*****] Усложнение. Вам известно, что (в перспективе) подобных спецификаций может быть много. Не две, а пять или десять
 
-# # Как улучшить свой код в этом случае, сделать его легко расширяемым?
+# from os.path import join
 
+# datapath = join(".", "семинары")
+# filename = join(datapath, "7seminar.csv")
+
+# with open(filename, mode='r', encoding='utf-8') as file:
+#    data = [el.strip().split('#') for el in file]
+#    new_fio = [f'{surname}{name[0]}.{parent[0]}.' for surname, name, parent in data]
+
+# print(*new_fio)
+
+# resultpath = join('.', 'семинары')
+# resultfilename = join(resultpath, "7result.csv")
+
+# with open(resultfilename, mode='w', encoding='utf-8') as file:
+#    [file.write(el + '\n') for el in new_fio]
+# # Как улучшить свой код в этом случае, сделать его легко расширяемым?
+# 
 # # №7.3[###]. Имея список вида [['Иванов', 'Иван', 'Иванович'], ['Петров', 'Петр', 'Петрович']...]
 
 # # преобразовать его в списки вида
@@ -149,6 +186,88 @@ print (new_data)
 # 2) ['Иванов И.И.', 'Петров П.П.', 'Соколов И.Г.'...]
 
 # с использованием Comprehension; Comprehension + функция; Comprehension + lambda; map
+# Comprehension
+# from os.path import join
+# def names():
+#      pass
+#   # raise NotImplementedError  для более красивого пропускания чем строчка выше
+# datapath = join(".", "семинары")
+# filename = join(datapath, "7seminar.csv")
+
+# with open(filename, mode='r', encoding='utf-8') as file:
+#    data = [el.strip().split('#') for el in file]
+#    new_fio = [f'{surname}{name[0]}.{parent[0]}.' for surname, name, parent in data]
+# print(*new_fio)
+
+# Comprehension + функция
+# from os.path import join
+# def names(user_data:list)->str:
+#    fio_string=user_data
+#    surname,name,parent=user_data
+#    return f'{surname}{name[0]}.{parent[0]}.'
+
+   
+   
+# datapath = join(".", "семинары")
+# filename = join(datapath, "7seminar.csv")
+
+# with open(filename, mode='r', encoding='utf-8') as file:
+#    data = [el.strip().split('#') for el in file]
+#    new_fio = [names(list_data_user) for list_data_user in data]
+# print(*new_fio)
+
+# Comprehension + lambda
+
+# from os.path import join
+
+# def names(user_data: list) -> str:
+#     fio_string = user_data
+#     surname, name, parent = user_data
+#     return f'{surname}{name[0]}.{parent[0]}.'
+
+# def names_lambda(surname: str, name: str, parent: str) -> str:
+#     return f'{surname}{name[0]}.{parent[0]}.'
+
+# datapath = join(".", "семинары")
+# filename = join(datapath, "7seminar.csv")
+
+# with open(filename, mode='r', encoding='utf-8') as file:
+    
+#     data = [el.strip().split('#') for el in file]
+#     new_fio=[(lambda surname,name,parent:f'{surname}{name[0]}.{parent[0]}.')(surname,name,parent) for surname,name,parent in data]
+    
+#    #  new_fio = [names_lambda(*list_data_user) for list_data_user in data] 1 вариант
+   
+
+
+# print(*new_fio)
+
+# map
+# from os.path import join
+
+# def names_comprehention_one(user_data: list) -> str:
+#     fio_string = user_data
+#     surname, name, parent = user_data
+#     return f'{surname}{name[0]}.{parent[0]}.'
+
+# def names_comprehention_two(surname: str, name: str, parent: str) -> str:
+#     return f'{surname}{name[0]}.{parent[0]}.'
+
+# datapath = join(".", "семинары")
+# filename = join(datapath, "7seminar.csv")
+
+# with open(filename, mode='r', encoding='utf-8') as file:
+    
+#     data = [el.strip().split('#') for el in file]
+#     new_fio = [*map (names_comprehention_one, data)]
+    
+#    #  new_fio = [names_lambda(*list_data_user) for list_data_user in data] 1 вариант
+   
+
+
+# print(*new_fio)
+# print(type(new_fio))
+
 
 # [**] Усложнение. Дополнить обработку списка условием: Выбираем только те элементы, в которых первая буква П
 
@@ -187,3 +306,20 @@ print (new_data)
 # same_by(lambda x: x[0], ["qz", "qr1", "qz", "qi", "qz", "qs", "qz", "qh"]) -> True
 
 # [*] Усложнение. Не используйте цикл в функции
+
+# def same_by(func, objects):
+#    if len(objects)==0: return None
+#    for idx in range(len(objects)):
+#       if func(objects[idx])!=func(objects[idx-1]): return False
+#    return True
+
+# print(same_by(lambda x: x % 2, [0, 2, 10, 12]))
+
+
+# [*] Усложнение. Не используйте цикл в функции
+def same_by(func, objects):
+   if len(objects)==0: return None
+   if len(set(map(func,objects)))==1:return True
+   return False
+
+print(same_by(lambda x: x % 2, [1, 2, 10, 12]))
